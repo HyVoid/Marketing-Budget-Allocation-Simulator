627cc0ff203bead83882b54f6265145bab2739e9
alex hong <hongyu12003@163.com>
2026-07-01 16:30:42 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: process.env.GITHUB_ACTIONS ? '/Marketing-Budget-Allocation-Simulator/' : '/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
